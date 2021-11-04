83a93b3fa6054d76c8b6a0682c4b07650c46d032
Robin Lovelace <rob00x@gmail.com>
2021-11-04 10:19:36 +0000

    Update with region_name variable

--- test-code/cycleways.R
+++ test-code/cycleways.R
@@ -3,6 +3,8 @@
 library(osmextract) # cran version
 library(tidyverse)
 
-# osm_highways = osmextract::oe_get_network(place = "west yorkshire", mode = "cycling")
+region_name = "isle of wight" # region to get cycleways for
+
+osm_highways = osmextract::oe_get_network(place = region_name, mode = "cycling")
 
 
