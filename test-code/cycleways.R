a5d25d315dd9f5b17a4194dcc7b2e362a0a280f2
Robin Lovelace <rob00x@gmail.com>
2021-11-04 10:12:28 +0000

    Add test code

---
+++ test-code/cycleways.R
@@ -0,0 +1,8 @@
+# Aim: get cycleways in west yorkshire
+
+library(osmextract) # cran version
+library(tidyverse)
+
+# osm_highways = osmextract::oe_get_network(place = "west yorkshire", mode = "cycling")
+
+
