b7cd8b83774e6d67f7765c247e04dff0b80e661d
Greta Timaite <gretatimaite@gmail.com>
2022-02-15 10:58:58 +0000

    leeds_central update
    Former-commit-id: 6375148a46b3823be309923b794a960620ce6f2e

--- test-code/central leeds.R
+++ test-code/central leeds.R
@@ -1,6 +1,7 @@
 # AIM: create an OSM dataset of central Leeds
 
-tags = c("kerb", "width", "sidewalk", "cycleway", "footway", "lit", "wheelchair", "maxspeed")
+tags = c("kerb", "width", "sidewalk", "cycleway", "footway", "lit", "wheelchair", "maxspeed", "foot", "access", "service")
+
 leeds = osmextract::oe_get(place = "Leeds",
                    provider = "bbbike",
                    force_vectortranslate = TRUE,
@@ -11,6 +12,8 @@ map_leeds = mapview::mapview(leeds)
 map_central_leeds = mapedit::editMap(map_leeds)
 box = sf::st_bbox(map_central_leeds$drawn$geometry)
 leeds_central = leeds[map_central_leeds$drawn$geometry, op=sf::st_within]
-
 saveRDS(leeds_central,
-        "leeds_central_14-02-2022.RDS")
+        "leeds_central_15-02-2022.RDS")
+
+
+
