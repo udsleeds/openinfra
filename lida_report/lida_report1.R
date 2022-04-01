a7676a1f60623b96bd1112b8a40fce3f015098d7
Greta Timaite <gretatimaite@gmail.com>
2022-04-01 18:26:06 +0100

    plots for each region
    Former-commit-id: be6178645a128a74dba0bfeda5411502a4f45be3

--- lida_report/lida_report1.R
+++ lida_report/lida_report1.R
@@ -520,4 +520,294 @@ wy_plot3 = wy_tagged_grouped_prop_im %>% filter(key %in% tags_plot_im) %>% selec
   theme(axis.text.x = element_text(angle = 90, vjust = .5, hjust=1)) 
 wy_plot3
 
+## GL
+gl_tagged_im = rbind(
+  gl_df %>% filter(!is.na(im_footway)) %>% mutate(key = "footway", value = im_footway, name = "gl"),
+  gl_df %>% filter(!is.na(im_footpath)) %>% mutate(key = "footpath", value = im_footpath, name = "gl"),
+  gl_df %>% filter(!is.na(im_kerb)) %>% mutate(key = "kerb", value = im_kerb, name = "gl"),
+  gl_df %>% filter(!is.na(im_light)) %>% mutate(key = "light", value = im_light, name = "gl"),
+  gl_df %>% filter(!is.na(im_maxspeed)) %>% mutate(key = "maxspeed", value = im_maxspeed, name = "gl"),
+  gl_df %>% filter(!is.na(im_tactile)) %>% mutate(key = "tactile_paving", value = im_tactile, name = "gl"),
+  gl_df %>% filter(!is.na(im_surface)) %>% mutate(key = "surface", value = im_surface, name = "gl")
+) 
 
+gl_df_cat = gl_df %>% 
+  mutate(
+    bicycle_cat = case_when(
+      str_detect(bicycle, "designated|yes")  ~ "yes/designated",
+      str_detect(bicycle, "no|dismount")  ~ "no",
+      str_detect(bicycle, "destin|permis|priva|unknown")~ "maybe"),
+    
+    wheelchair_cat = case_when(
+      str_detect(wheelchair, "bad|limit|permis") ~ ",aybe",
+      str_detect(wheelchair, "desig|yes") ~ "yes",
+      str_detect(wheelchair, "no") ~ "no"
+    ),
+    foot_cat = case_when(
+      str_detect(foot, "designated|yes")  ~ "yes/designated",
+      str_detect(foot, "no|disc")  ~ "no",
+      str_detect(foot, "cust|deli|dest|emerg|limit|permis|permit|priv|unknown")  ~ "maybe"
+    ),
+    footway_cat = case_when(
+      str_detect(footway, "access|alley|left|link|traffic") ~ "other",
+      str_detect(footway, "no") ~ "no",
+      str_detect(footway, "yes|sidewalk") ~ "yes/sidewalk",
+    ),
+    lit_cat = case_when(
+      str_detect(lit, "af|auto|dis|interval|sep|suns|sunr")~ "other",
+      str_detect(lit, "limit")~ "limited",
+      str_detect(lit, "yes")~ "yes",
+      str_detect(lit, "no")~ "no"
+    ),
+    maxspeed_cl = maxspeed %>% 
+      parse_number(),
+    maxspeed_cat = case_when(
+      maxspeed_cl > 1 & maxspeed_cl <= 20 ~ "1-20 mph",
+      maxspeed_cl > 20 & maxspeed_cl <= 40~ "21-40 mph",
+      maxspeed_cl > 40 & maxspeed_cl <= 60~ "41-60 mph",
+      maxspeed_cl > 60 ~ "61 mph <"
+    ),
+    cycleway_cat = case_when(
+      str_detect(cycleway, "buff|lane|segr|sep|track") ~ "separate",
+      str_detect(cycleway, "no") ~ "no",
+      str_detect(cycleway, "share") ~ "shared",
+      str_detect(cycleway, "cross|cyclestreet|left|opp|path|side|yes|unm") ~ "other"
+    ),
+    kerb_cat = case_when(
+      str_detect(kerb, "fl")~ "flush",
+      str_detect(kerb, "low")~ "lowered",
+      str_detect(kerb, "raise")~ "raised",
+      str_detect(kerb, "yes|no")~ "other"
+    ),
+    width_cl = width %>% parse_number(),
+    width_cat = case_when(
+      width_cl > 0 & width_cl < 1.5 ~ "< 1.5m",
+      width_cl <= 1.5 & width_cl <= 2 ~ "1.5m - 2m",
+      width_cl > 2 ~ "2m <"
+    ),
+    sidewalk_cat = case_when(
+      str_detect(sidewalk, "both")~ "both",
+      str_detect(sidewalk, "left")~ "left",
+      str_detect(sidewalk, "right")~ "right",
+      str_detect(sidewalk, "no|none")~ "no",
+      str_detect(sidewalk, "separate")~ "separated",
+      str_detect(sidewalk, "yes|mapped")~ "yes",
+      str_detect(sidewalk, "cross")~ "other")
+  )
+
+
+gl_tagged = rbind(
+  gl_df_cat %>% filter(!is.na(wheelchair_cat)) %>% mutate(key = "wheelchair", value = wheelchair_cat, name = "gl"),
+  gl_df_cat %>% filter(!is.na(foot_cat)) %>% mutate(key = "foot", value = foot_cat, name = "gl"),
+  gl_df_cat %>% filter(!is.na(footway_cat)) %>% mutate(key = "footway", value = footway_cat, name = "gl"),
+  gl_df_cat %>% filter(!is.na(lit_cat)) %>% mutate(key = "lit", value = lit_cat, name = "gl"),
+  gl_df_cat %>% filter(str_detect(highway, "footway|living_street|path|pedestrian|steps|cycle")) %>%
+    mutate(key = "highway", value = highway, name = "gl"),
+  gl_df_cat %>% filter(!is.na(maxspeed_cat)) %>% mutate(key = "maxspeed", value = maxspeed_cat, name = "gl"),
+  gl_df_cat %>% filter(!is.na(bicycle_cat)) %>% mutate(key = "bicycle", value = bicycle_cat, name = "gl"),
+  gl_df_cat %>% filter(!is.na(cycleway_cat)) %>% mutate(key = "cycleway", value = cycleway_cat, name = "gl"),
+  gl_df_cat %>% filter(!is.na(kerb_cat)) %>% mutate(key = "kerb", value = kerb_cat, name = "gl"),
+  gl_df_cat %>% filter(!is.na(width_cat)) %>% mutate(key = "width", value = width_cat, name = "gl"),
+  gl_df_cat %>% filter(!is.na(sidewalk_cat)) %>% mutate(key = "sidewalk", value = sidewalk_cat, name = "gl")
+) 
+
+gl_tagged_grouped = gl_tagged %>% 
+  group_by(key, value) %>% 
+  mutate(n = n()) %>% 
+  ungroup() 
+gl_tagged_grouped_im = gl_tagged_im %>% 
+  group_by(key, value) %>% 
+  mutate(n = n()) %>% 
+  ungroup() 
+
+# plot 1
+gl_tagged_grouped_prop = gl_tagged_grouped %>% 
+  group_by(highway) %>% 
+  summarize(n = n(),
+            Proportion = n / nrow(gl_df))
+
+gl_plot1 = gl_tagged_grouped_prop %>%  filter(str_detect(highway, "foot|cycle|ped|steps|living"))   %>% 
+  ggplot( aes(x = highway,
+              y = Proportion)) +
+  scale_y_continuous(labels = scales::percent) +
+  geom_bar(stat = "identity") 
+gl_plot1
+
+# plot 2
+
+tags_plot = c("bicycle", "cycleway", "foot", "footway")
+gl_tagged_grouped_prop2 = gl_tagged_grouped %>% 
+  group_by(value, key) %>% 
+  summarize(n = n(),
+            Proportion = n / nrow(gl_df))
+gl_plot2 = gl_tagged_grouped_prop2 %>% filter(key %in% tags_plot) %>% select(key, Proportion) %>% 
+  ggplot( aes(x = value,
+              y = Proportion)) +
+  geom_bar(stat = "identity") +
+  facet_wrap(~key, scales = "free_x") +
+  scale_y_continuous(labels = scales::percent) +
+  theme_bw() +
+  theme(axis.text.x = element_text(angle = 90, vjust = .5, hjust=1)) 
+gl_plot2
+
+# plot 3
+tags_plot_im = c("footway", "footpath", "kerb", "maxspeed")
+gl_tagged_grouped_prop_im = gl_tagged_grouped_im %>% 
+  group_by(value, key) %>% 
+  summarize(n = n(),
+            Proportion = n / nrow(gl_df))
+gl_plot3 = gl_tagged_grouped_prop_im %>% filter(key %in% tags_plot_im) %>% select(key, Proportion) %>% 
+  ggplot( aes(x = value,
+              y = Proportion)) +
+  geom_bar(stat = "identity") +
+  facet_wrap(~key, scales = "free_x") +
+  scale_y_continuous(labels = scales::percent) +
+  theme_bw() +
+  theme(axis.text.x = element_text(angle = 90, vjust = .5, hjust=1)) 
+gl_plot3
+
+## Mers
+mers_tagged_im = rbind(
+  mers_df %>% filter(!is.na(im_footway)) %>% mutate(key = "footway", value = im_footway, name = "mers"),
+  mers_df %>% filter(!is.na(im_footpath)) %>% mutate(key = "footpath", value = im_footpath, name = "mers"),
+  mers_df %>% filter(!is.na(im_kerb)) %>% mutate(key = "kerb", value = im_kerb, name = "mers"),
+  mers_df %>% filter(!is.na(im_light)) %>% mutate(key = "light", value = im_light, name = "mers"),
+  mers_df %>% filter(!is.na(im_maxspeed)) %>% mutate(key = "maxspeed", value = im_maxspeed, name = "mers"),
+  mers_df %>% filter(!is.na(im_tactile)) %>% mutate(key = "tactile_paving", value = im_tactile, name = "mers"),
+  mers_df %>% filter(!is.na(im_surface)) %>% mutate(key = "surface", value = im_surface, name = "mers")
+) 
+
+mers_df_cat = mers_df %>% 
+  mutate(
+    bicycle_cat = case_when(
+      str_detect(bicycle, "designated|yes")  ~ "yes/designated",
+      str_detect(bicycle, "no|dismount")  ~ "no",
+      str_detect(bicycle, "destin|permis|priva|unknown")~ "maybe"),
+    
+    wheelchair_cat = case_when(
+      str_detect(wheelchair, "bad|limit|permis") ~ ",aybe",
+      str_detect(wheelchair, "desig|yes") ~ "yes",
+      str_detect(wheelchair, "no") ~ "no"
+    ),
+    foot_cat = case_when(
+      str_detect(foot, "designated|yes")  ~ "yes/designated",
+      str_detect(foot, "no|disc")  ~ "no",
+      str_detect(foot, "cust|deli|dest|emerg|limit|permis|permit|priv|unknown")  ~ "maybe"
+    ),
+    footway_cat = case_when(
+      str_detect(footway, "access|alley|left|link|traffic") ~ "other",
+      str_detect(footway, "no") ~ "no",
+      str_detect(footway, "yes|sidewalk") ~ "yes/sidewalk",
+    ),
+    lit_cat = case_when(
+      str_detect(lit, "af|auto|dis|interval|sep|suns|sunr")~ "other",
+      str_detect(lit, "limit")~ "limited",
+      str_detect(lit, "yes")~ "yes",
+      str_detect(lit, "no")~ "no"
+    ),
+    maxspeed_cl = maxspeed %>% 
+      parse_number(),
+    maxspeed_cat = case_when(
+      maxspeed_cl > 1 & maxspeed_cl <= 20 ~ "1-20 mph",
+      maxspeed_cl > 20 & maxspeed_cl <= 40~ "21-40 mph",
+      maxspeed_cl > 40 & maxspeed_cl <= 60~ "41-60 mph",
+      maxspeed_cl > 60 ~ "61 mph <"
+    ),
+    cycleway_cat = case_when(
+      str_detect(cycleway, "buff|lane|segr|sep|track") ~ "separate",
+      str_detect(cycleway, "no") ~ "no",
+      str_detect(cycleway, "share") ~ "shared",
+      str_detect(cycleway, "cross|cyclestreet|left|opp|path|side|yes|unm") ~ "other"
+    ),
+    kerb_cat = case_when(
+      str_detect(kerb, "fl")~ "flush",
+      str_detect(kerb, "low")~ "lowered",
+      str_detect(kerb, "raise")~ "raised",
+      str_detect(kerb, "yes|no")~ "other"
+    ),
+    width_cl = width %>% parse_number(),
+    width_cat = case_when(
+      width_cl > 0 & width_cl < 1.5 ~ "< 1.5m",
+      width_cl <= 1.5 & width_cl <= 2 ~ "1.5m - 2m",
+      width_cl > 2 ~ "2m <"
+    ),
+    sidewalk_cat = case_when(
+      str_detect(sidewalk, "both")~ "both",
+      str_detect(sidewalk, "left")~ "left",
+      str_detect(sidewalk, "right")~ "right",
+      str_detect(sidewalk, "no|none")~ "no",
+      str_detect(sidewalk, "separate")~ "separated",
+      str_detect(sidewalk, "yes|mapped")~ "yes",
+      str_detect(sidewalk, "cross")~ "other")
+  )
+
+
+mers_tagged = rbind(
+  mers_df_cat %>% filter(!is.na(wheelchair_cat)) %>% mutate(key = "wheelchair", value = wheelchair_cat, name = "mers"),
+  mers_df_cat %>% filter(!is.na(foot_cat)) %>% mutate(key = "foot", value = foot_cat, name = "mers"),
+  mers_df_cat %>% filter(!is.na(footway_cat)) %>% mutate(key = "footway", value = footway_cat, name = "mers"),
+  mers_df_cat %>% filter(!is.na(lit_cat)) %>% mutate(key = "lit", value = lit_cat, name = "mers"),
+  mers_df_cat %>% filter(str_detect(highway, "footway|living_street|path|pedestrian|steps|cycle")) %>%
+    mutate(key = "highway", value = highway, name = "mers"),
+  mers_df_cat %>% filter(!is.na(maxspeed_cat)) %>% mutate(key = "maxspeed", value = maxspeed_cat, name = "mers"),
+  mers_df_cat %>% filter(!is.na(bicycle_cat)) %>% mutate(key = "bicycle", value = bicycle_cat, name = "mers"),
+  mers_df_cat %>% filter(!is.na(cycleway_cat)) %>% mutate(key = "cycleway", value = cycleway_cat, name = "mers"),
+  mers_df_cat %>% filter(!is.na(kerb_cat)) %>% mutate(key = "kerb", value = kerb_cat, name = "mers"),
+  mers_df_cat %>% filter(!is.na(width_cat)) %>% mutate(key = "width", value = width_cat, name = "mers"),
+  mers_df_cat %>% filter(!is.na(sidewalk_cat)) %>% mutate(key = "sidewalk", value = sidewalk_cat, name = "mers")
+) 
+
+mers_tagged_grouped = mers_tagged %>% 
+  group_by(key, value) %>% 
+  mutate(n = n()) %>% 
+  ungroup() 
+mers_tagged_grouped_im = mers_tagged_im %>% 
+  group_by(key, value) %>% 
+  mutate(n = n()) %>% 
+  ungroup() 
+
+# plot 1
+mers_tagged_grouped_prop = mers_tagged_grouped %>% 
+  group_by(highway) %>% 
+  summarize(n = n(),
+            Proportion = n / nrow(mers_df))
+
+mers_plot1 = mers_tagged_grouped_prop %>%  filter(str_detect(highway, "foot|cycle|ped|steps|living"))   %>% 
+  ggplot( aes(x = highway,
+              y = Proportion)) +
+  scale_y_continuous(labels = scales::percent) +
+  geom_bar(stat = "identity") 
+mers_plot1
+
+# plot 2
+
+tags_plot = c("bicycle", "cycleway", "foot", "footway")
+mers_tagged_grouped_prop2 = mers_tagged_grouped %>% 
+  group_by(value, key) %>% 
+  summarize(n = n(),
+            Proportion = n / nrow(mers_df))
+mers_plot2 = mers_tagged_grouped_prop2 %>% filter(key %in% tags_plot) %>% select(key, Proportion) %>% 
+  ggplot( aes(x = value,
+              y = Proportion)) +
+  geom_bar(stat = "identity") +
+  facet_wrap(~key, scales = "free_x") +
+  scale_y_continuous(labels = scales::percent) +
+  theme_bw() +
+  theme(axis.text.x = element_text(angle = 90, vjust = .5, hjust=1)) 
+mers_plot2
+
+# plot 3
+tags_plot_im = c("footway", "footpath", "kerb", "maxspeed")
+mers_tagged_grouped_prop_im = mers_tagged_grouped_im %>% 
+  group_by(value, key) %>% 
+  summarize(n = n(),
+            Proportion = n / nrow(mers_df))
+mers_plot3 = mers_tagged_grouped_prop_im %>% filter(key %in% tags_plot_im) %>% select(key, Proportion) %>% 
+  ggplot( aes(x = value,
+              y = Proportion)) +
+  geom_bar(stat = "identity") +
+  facet_wrap(~key, scales = "free_x") +
+  scale_y_continuous(labels = scales::percent) +
+  theme_bw() +
+  theme(axis.text.x = element_text(angle = 90, vjust = .5, hjust=1)) 
+mers_plot3
