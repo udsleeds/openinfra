71645e5b6be75050c2507d90a38764d750bc5bb1
hulsiejames <hulse.jjc@sky.com>
2022-06-28 16:21:09 +0100

    replaced dplyr pipe (%>%) with base pipe (|>) due to workflow error  could not find function %

--- R/recode_road_class.R
+++ R/recode_road_class.R
@@ -33,7 +33,7 @@
 #' @export
 recode_road_class <- function(osm_sf) {
   # Created road_class columns
-  osm_recat = osm_sf %>%
+  osm_recat = osm_sf |>
     # Creates road_class column
     dplyr::mutate(road_class = dplyr::case_when(
       # (7) - Motorways
@@ -50,7 +50,7 @@ recode_road_class <- function(osm_sf) {
       highway %in% c("living_street", "residential", "unclassified") ~ "1",
       # (0) - Traffic-free Paths
       highway == "cycleway" ~ "0"
-    )) %>%
+    )) |>
     
     # Creates road_description columns
     dplyr::mutate(road_desc = dplyr::case_when(
@@ -68,7 +68,7 @@ recode_road_class <- function(osm_sf) {
       highway %in% c("living_street", "residential", "unclassified") ~ "Residential / Local Roads",
       # (0) - Traffic-free Paths
       highway == "cycleway" ~ "Cycleway"
-    )) %>%
+    )) |>
     
     # Removes features that have not been recodeed to a road_class value
     filter(!is.na(road_class))
