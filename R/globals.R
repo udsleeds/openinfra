e647951a7f07128cf0058445e46f8b070c35a4a6
hulsiejames <hulse.jjc@sky.com>
2022-07-04 17:07:33 +0100

    Created R/globals.R to fix error CMD-check error ❯ Undefined global functions or variables: road_class

---
+++ R/globals.R
@@ -0,0 +1,4 @@
+# Script containing varibales listed in devtools::check ) stage [❯ checking R code for possible problems ... NOTE]
+# created as a suggested fix from (https://community.rstudio.com/t/how-to-solve-no-visible-binding-for-global-variable-note/28887)
+
+utils::globalVariables(c("road_class"))
