e9fca8bb6719acc3831de251f28aa254c703b6c1
hulsiejames <hulse.jjc@sky.com>
2022-07-19 14:50:28 +0100

    uploading new dataset containing is_lit_led tag

--- R/data.R
+++ R/data.R
@@ -13,3 +13,20 @@
 #' library(sf)
 #' extended_data   # Lazy Loading. Data becomes visible as soon as it is requested. 
 "extended_data"
+
+
+#' OSM infrastructure data used in creation of transport infrastructure data packs.
+#' 
+#' Contains all columns required for IM function, oi_recode_max_speed, oi_recode_road_, oi_active_cycle, oi_is_lit.
+#' 
+#' @docType data
+#' @keywords dataset
+#' 
+#' @format A data frame with 29,911 features described by 29 variables. 
+#' 
+#' @source {[generate_example_data.R](https://github.com/udsleeds/openinfra/blob/main/test-code/generate_example_data.R)}
+#'
+#' @examples
+#' library(sf)
+#' extended_data   # Lazy Loading. Data becomes visible as soon as it is requested. 
+"example_data"
