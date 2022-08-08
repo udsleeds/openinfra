eaa2a88a92006729036791bf84ed7761f1184d2e
hulsiejames <hulse.jjc@sky.com>
2022-08-08 17:21:33 +0100

    script investigating LTN1_20 compliance through analysis of OSM ways

---
+++ test-code/LTN_compliance_tests.R
@@ -0,0 +1,5 @@
+# Script for test-code on LTN1/20 compliance - will investigate the physical 
+# requirements (width, road separation etc.) of cycleways to see whether or not 
+# they comply with LTN1/20 guide. Of particular use is the LTN1/20 guidance: 
+# https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/951074/cycle-infrastructure-design-ltn-1-20.pdf
+# specifially, chapters 4 & 5.
