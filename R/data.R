069e1f5a83d16b8a37e7a830c811f3399b208969
hulsiejames <hulse.jjc@sky.com>
2022-08-15 17:06:32 +0100

    included new data to be used in SOTM presentation & workshops

--- R/data.R
+++ R/data.R
@@ -13,3 +13,19 @@
 #' library(sf)
 #' example_data   # Lazy Loading. Data becomes visible as soon as it is requested. 
 "example_data"
+
+#' OSM infrastructure data used in creation of transport infrastructure data packs.
+#' 
+#' 
+#' 
+#' @docType data
+#' @keywords dataset
+#' 
+#' @format A data frame with 11,741 features described by 34 variables. 
+#' 
+#' @source {[generate_example_data.R](https://github.com/udsleeds/openinfra/blob/main/test-code/generate_example_data.R)}
+#'
+#' @examples
+#' library(sf)
+#' sotm_data   # Lazy Loading. Data becomes visible as soon as it is requested. 
+"sotm_data"
