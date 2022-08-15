66d82d9adbf1699d5cfec9be2d075cf5fbb619c2
hulsiejames <hulse.jjc@sky.com>
2022-08-15 15:40:46 +0100

    updated recode_road function to not auto delete uncoded roads

--- R/recode_road_class.R
+++ R/recode_road_class.R
@@ -18,6 +18,7 @@
 #' 0 | Traffic-free Paths | highway = cycleway
 #'
 #' @param osm_sf - A `sf` and `data.frame` object containing OpenStreetMap infrastructure data, obtained from the [`osmextract`](https://github.com/ropensci/osmextract) function.
+#' @param del If `TRUE`, features not re-coded to one of the 8 road classifications are removed. `FALSE` by default.
 #' @return  The \code{osm_sf} simple features data frame is returned with additional columns road_class and road_desc based on Chan and Cooper's road classifications.
 #' @export oi_recode_road_class
 #' 
@@ -28,7 +29,7 @@
 #' u_data_small = paste0("https://github.com/udsleeds/openinfra/releases",
 #'                       "/download/v0.2/30_06_22_bbbike_LCC_func_example_5_75km.geojson")
 #' internal_data = example_data
-#' output = oi_recode_road_class(internal_data)
+#' output = oi_recode_road_class(internal_data, del = FALSE)
 
 #' # Quick plot:
 #'  plot(output["oi_road_desc"], key.pos = 1)
@@ -39,7 +40,7 @@
 #' #  tmap::tm_lines(col = "road_desc", title.col = "Road class") +
 #' #  tmap::tm_layout(legend.bg.color = "white")
 
-oi_recode_road_class = function(osm_sf) {
+oi_recode_road_class = function(osm_sf, del = FALSE) {
   # browser() Uncomment this to perform function debugging 
   
   # Created road_class columns
@@ -66,7 +67,7 @@ oi_recode_road_class = function(osm_sf) {
     dplyr::mutate(oi_road_desc = dplyr::case_when(
       # (7) - Motorways
       highway %in% c("motorway", "motorway_link") ~ "Motorways",
-      # (6/5) - Dual Carriageways resi & non-resi
+      # (6/5) - Dual Carriageways, residential & non-residential
       (highway %in% c("trunk", "trunk_link")) & (oneway %in% c("yes", "-1", "reversible", "alternating")) ~ "Dual Carriageways (R&NR)",
       # (4) - Primary Roads
       highway %in% c("primary", "primary_link") | (highway %in% c("trunk", "trunk_link")) & (! oneway %in% c("yes", "-1", "reversible", "alternating")) ~ "Primary Roads",
@@ -78,9 +79,13 @@ oi_recode_road_class = function(osm_sf) {
       highway %in% c("living_street", "residential", "unclassified") ~ "Residential / Local Roads",
       # (0) - Traffic-free Paths
       highway == "cycleway" ~ "Cycleway"
-    )) %>%
+    )) #%>%
     
-    # Removes features that have not been recodeed to a road_class value
-    dplyr::filter(!is.na(oi_road_class))
+  # If remove == FALSE, remove features that have not been re-coded to a road_class value
+  if (del){
+    osm_recat = osm_recat %>% dplyr::filter(!is.na(oi_road_class))
+  } 
+    
+  
   return(osm_recat)
 }
