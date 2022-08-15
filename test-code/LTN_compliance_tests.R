3af9192a0e5443c52c95663ee2051915b4d6b593
hulsiejames <hulse.jjc@sky.com>
2022-08-15 16:44:33 +0100

    scripts investigating compliance with LTN1/20 function

--- test-code/LTN_compliance_tests.R
+++ test-code/LTN_compliance_tests.R
@@ -15,9 +15,11 @@ pkgs = c("sf",
 lapply(pkgs, library, character.only = TRUE)[length(pkgs)]
 
 required_tags = c("foot", "bicycle", "access", "service", "maxspeed", "oneway",
-                  "kerb", "footway", "sidewalk", "cycleway", "segregated", "highway",
-                  "crossing", "lit", "tactile_paving", "surface", "smoothness", "width",
-                  "est_width", "lit_by_led", "boundary", "admin_level", "name", "ref")
+                  "kerb", "footway", "sidewalk", "cycleway", "segregated",
+                  "highway", "crossing", "lit", "tactile_paving", "surface",
+                  "smoothness", "width", "est_width", "lit_by_led", "boundary",
+                  "admin_level", "name", "ref", "cycleway:left",
+                  "cycleway:right")
 
 # Get data ----------------------------------------------------------------
 
@@ -28,15 +30,89 @@ leeds_lad_poly = LADs %>%
 
 # Request data specify leeds_lad_poly as palce
 leeds_first_network = osmextract::oe_get(
-  place = leeds_lad_poly,
+  place = "Leeds",
   layer = "lines",
   boundary_type = "clipsrc",
   extra_tags = required_tags
 )
 
-### THIS MIGHT NO LONGER EB NEEDED THNKS TO "clipsrc" WITHIN OE_GET
 # Re sample the network to contain ONLY the network covered by leeds_lad_poly
 leeds_network = leeds_first_network[leeds_lad_poly, ]
 
+leeds_osmex_cycling = osmextract::oe_get_network(
+  mode = "cycling",
+  place = "Leeds",
+  provider = "bbbike",
+  extra_tags = required_tags,
+  force_vectortranslate = TRUE
+)
 
 
+# Tag Distribution Analysis ----------------------------------------------------------------
+cycleway_values = as.data.frame(table(leeds_network$cycleway))
+
+no_NA_cycleway_lanes_leeds = leeds_network %>% dplyr::filter(! is.na(cycleway))
+
+width_vc = as.data.frame(table(no_NA_cycleway_lanes_leeds$width))
+no_NA_width_cycleway_leeds = no_NA_cycleway_lanes_leeds %>% dplyr::filter(! is.na(width))
+
+
+# oi_classify_cycle_ways --------------------------------------------------
+
+################################################################################
+# Create a function that can classify a way used for cycling as being either:  #
+#     - Fully Kerbed Cycle Track                                               #
+#     - Stepped Cycle Track/Lance                                              #
+#     - Lightly Segregated Cycle Track/Lane                                    #
+#     - Cycle Lane (Mandatory & Advisory)                                      #
+#     - Mixed Traffic (Motor (& maybe pedestrian?) )                           #
+#                                                                              #
+# Way classification will be determined by assessing the tags assigned to each #
+# way and comparing those against definitions proposed by cyclestreets.net     #
+# (https://bikedata.cyclestreets.net/tflcid/conversion/#cycle_lane_track) for  #
+# converting London CID to OSM.                                                #
+################################################################################
+# Due to issues identifying levels of segregation through OSM tags, due to a   #
+# lack of cycle infrastructure tagging consistency [example]. As such we will  #
+# catagorise infrastructure as either:                                         #
+#     - Segregated (Fully Kerbed/Stepped/Light)                                #
+#     - Cycle Lanes (Mandatory/Advisory) <-- On Carriage Ways                  #
+#     - Mixed Traffic (No designated cycling route, but cyclists are lgeally   #
+#                      allowed to travel on most UK roads - See below url for  #
+#                      more info.https://tinyurl.com/OSMaccess)                #
+#                                                                              #
+#                                                                              #
+################################################################################
+leeds_mixed_traffic = leeds_osmex_cycling %>% dplyr::filter(
+  cycleway %in% c("no", "none", "opposite")
+)
+
+
+leeds_cycle_lanes = leeds_osmex_cycling %>% 
+  # Capture Obvious cycle lanes
+  dplyr::filter(cycleway %in% c("lane")) %>% 
+  # Capture more obscure cycle lanes
+  dplyr::filter(cycleway_left %in% c("lane")) %>% 
+  dplyr::filter(cycleway_right %in% c("lane"))
+
+
+# Each application of dplyr::filter REMOVES features, thus you cannot apply it three
+# times to form a network, the network will be whatever the last filter was.
+
+leeds_segregated = leeds_osmex_cycling %>% 
+  # Captures obvious track lanes - separated by definition
+  dplyr::filter(cycleway %in% c("track")) %>% #& (cycleway_left %in% c("track")) & (cycleway_right %in% c("track")) )
+  # Captures more obscure track lanes 
+  #dplyr::filter(cycleway_left %in% c("track")) #%>%
+  dplyr::filter(cycleway_right %in% c("track"))
+  
+visualise = tmap::tm_shape(leeds_cycle_lanes) + 
+    tmap::tm_lines(col = "highway") + 
+  
+  tmap::tm_shape(leeds_mixed_traffic) + 
+    tmap::tm_lines(col = "highway") + 
+  
+  tmap::tm_shape(leeds_segregated) + 
+    tmap::tm_lines(col = "highway")
+
+visualise
