d30b50e57cbde3763ed67a3b8e40d52734a54074
Robin Lovelace <rob00x@gmail.com>
2022-08-20 16:43:14 +0100

    Finish example from ohsome

--- test-code/ohsome.R
+++ test-code/ohsome.R
@@ -7,7 +7,6 @@ library(tidyverse)
 leeds_boundary = osmdata::getbb(place_name = "leeds", format_out = "sf_polygon", limit = 1)
 q = ohsome_elements_count(leeds_boundary, filter = "highway=cycleway")
 ohsome_post(query = q)
-library(ggplot2)
 
 cycleway_count = q |> 
   set_time("2009/2022/P1M") |>
@@ -29,3 +28,25 @@ cycleway_length |>
   geom_line() +
   ylab(label = "Cycleway length (km)")
 
+transport_regions = sf::read_sf("https://github.com/udsleeds/openinfra/raw/main/data-small/regions_new.geojson")
+
+i = 1
+cycleway_lengths = NULL
+transport_regions_selected = transport_regions |> 
+  filter(str_detect(string = Region_name, pattern = "Yorks|Manc|Lond|Brist"))
+# for(i in 1:3) {
+for(i in seq(nrow(transport_regions))) {
+  message("Getting data for ", transport_regions$Region_name[i])
+  region = transport_regions$geometry[i]
+  cycleway_length = leeds_boundary |> 
+    ohsome_elements_length(filter = "highway=cycleway") |> 
+    set_time("2009/2022/P1M") |>
+    ohsome_post() |> 
+    mutate(region = transport_regions$Region_name[i])
+  cycleway_lengths = bind_rows(cycleway_lengths, cycleway_length)
+}
+
+cycleway_lengths |>
+  ggplot(aes(x = timestamp, y = value / 1000, fill = region)) +
+  geom_area() +
+  ylab(label = "Cycleway length (km)")
