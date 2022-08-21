155cfd25ec3d9429df7a3b9a537ad20b48c564fc
hulsiejames <hulse.jjc@sky.com>
2022-08-21 22:47:49 +0100

    added oi_road_name required tags. #99

--- R/oi_road_names.R
+++ R/oi_road_names.R
@@ -6,11 +6,13 @@
 #'        OpenStreetMap infrastructure data. 
 #' @return an sf object with oi_road_name column added, indicating the name and
 #'   ref fields of the feature, if they are included.
-#' @details This fucntion analyses OSM features, specifically the `name` and 
+#' @details This function analyses OSM features, specifically the `name` and 
 #'   `ref` fields that contain a road name (i.e. Otley Road) and the road 
 #'   reference field (i.e A62). If both the `name` and `ref` appear, then 
 #'   `oi_road_name` will be `name | ref`, otherwise `oi_road_name` will be 
 #'   whichever field appears within the OSM data. 
+#'   
+#'   Note: the `osm_sf` must contain the following tags: `c("name", "ref")`
 #' @export oi_road_names
 #' @examples
 #' data = example_data
