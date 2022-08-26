80c72c6563a0e7cd61f26d4f75d73e45c692562e
hulsiejames <hulse.jjc@sky.com>
2022-08-26 18:23:17 +0100

    updated package data - now a lines dataset (default) and pois dataset

--- R/data.R
+++ R/data.R
@@ -1,30 +1,29 @@
-#' OSM infrastructure data used in creation of transport infrastructure data packs.
-#' 
-#' 
+#' OSM ways data used in the creation of transport infrastructure data packs.
 #' @docType data
 #' @keywords dataset
-#' 
-#' @format A data frame with 29,911 features described by 29 variables. 
-#' 
+#' @format A data frame of OSM ways.
 #' @source {[generate_example_data.R](https://github.com/udsleeds/openinfra/blob/main/test-code/generate_example_data.R)}
-#'
 #' @examples
 #' library(sf)
 #' example_data   # Lazy Loading. Data becomes visible as soon as it is requested. 
 "example_data"
 
-#' OSM infrastructure data used in creation of transport infrastructure data packs.
-#' 
-#' 
-#' 
+#' OSM infrastructure data used in SOTM2022 workshop & figures for SOTM2022. 
 #' @docType data
 #' @keywords dataset
-#' 
-#' @format A data frame with 11,741 features described by 34 variables. 
-#' 
+#' @format A data frame of OSM ways.
 #' @source {[generate_example_data.R](https://github.com/udsleeds/openinfra/blob/main/test-code/generate_example_data.R)}
-#'
 #' @examples
 #' library(sf)
 #' sotm_data   # Lazy Loading. Data becomes visible as soon as it is requested. 
 "sotm_data"
+
+#' OSM points data used in the creation of transport infrastructure data packs.
+#' @docType data
+#' @keywords dataset
+#' @format A dat frame of OSM ways.
+#' @source {[generate_example_data.R](https://github.com/udsleeds/openinfra/blob/main/test-code/generate_example_data.R)}
+#' @examples 
+#' library(sf)
+#' example_data_pois
+"example_data_pois"
