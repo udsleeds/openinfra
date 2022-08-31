11f5d9ba62c0eb3bcd9591e02475b6cb7cdafbb6
Robin Lovelace <rob00x@gmail.com>
2022-08-31 22:22:43 +0100

    Add abstract for #115

--- test-code/ohsome.R
+++ test-code/ohsome.R
@@ -32,13 +32,14 @@ transport_regions = sf::read_sf("https://github.com/udsleeds/openinfra/raw/main/
 
 i = 1
 cycleway_lengths = NULL
-transport_regions_selected = transport_regions |> 
-  filter(str_detect(string = Region_name, pattern = "Yorks|Manc|Lond|Brist"))
+transport_regions = transport_regions |> 
+  filter(str_detect(string = Region_name, pattern = "Yorks|Manc|Lond|Brist|West of|Camb|West Mid|Ox"))
 # for(i in 1:3) {
 for(i in seq(nrow(transport_regions))) {
+# for(i in seq(nrow(transport_regions))) {
   message("Getting data for ", transport_regions$Region_name[i])
   region = transport_regions$geometry[i]
-  cycleway_length = leeds_boundary |> 
+  cycleway_length = region |> 
     ohsome_elements_length(filter = "highway=cycleway") |> 
     set_time("2009/2022/P1M") |>
     ohsome_post() |> 
@@ -49,4 +50,10 @@ for(i in seq(nrow(transport_regions))) {
 cycleway_lengths |>
   ggplot(aes(x = timestamp, y = value / 1000, fill = region)) +
   geom_area() +
+  ylab(label = "Cycleway length (km)")
+
+cycleway_lengths |>
+  ggplot(aes(x = timestamp, y = value / 1000)) +
+  geom_area() +
+  facet_wrap(~region) +
   ylab(label = "Cycleway length (km)")
