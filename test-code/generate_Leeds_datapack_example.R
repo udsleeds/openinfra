de8a2545c48280e0a3dda451793533d792f504b7
hulsiejames <hulse.jjc@sky.com>
2022-09-05 15:00:00 +0100

    edit script for new data pack maps

--- test-code/generate_Leeds_datapack_example.R
+++ test-code/generate_Leeds_datapack_example.R
@@ -3,7 +3,7 @@
 
 # Setup -------------------------------------------------------------------
 
-remotes::install_github("udsleeds/openinfra")
+#remotes::install_github("udsleeds/openinfra")
 # Library Imports
 pkgs = c("sf",
          "osmextract",
@@ -17,7 +17,67 @@ lapply(pkgs, library, character.only = TRUE)[length(pkgs)]
 # Key parameters ----------------------------------------------------------
 
 region_name = "leeds"
-release_tag = "0.3"
+
+#required_tags = c(colnames(openinfra::sotm_data))
+required_tgas = c("foot", "bicycle", "access", "service", "maxspeed", "oneway",
+                  "kerb", "footway", "sidewalk", "cycleway", "segregated", "highway",
+                  "crossing", "lit", "tactile_paving", "surface", "smoothness", "width",
+                  "est_width", "lit_by_led", "ref")
+
+# Data acquisition ---------------------------------------------------------
+
+# Set up buffer
+leeds_centre_point = sf::st_sfc(sf::st_point(c(-1.549, 53.801)), 
+                              crs = "EPSG:4326")
+leeds_buffer = sf::st_buffer(leeds_centre_point, dist = 1500) #1500 metre radius
+
+test1 = oe_get_network(
+  place = region_name,
+  mode = "driving",
+  download_directory = "/home/james/Desktop/r_dl_tests",
+  extra_tags = required_tags
+)
+test_url = oe_match("Leeds")
+
+url = test_url$url
+
+test_dl = oe_download(file_url = url,
+                      download_directory = "/home/james/Desktop/r_dl_tests/")
+
+test2 = oe_read(
+  file_path = "/home/james/Desktop/r_dl_tests/geofabrik_Leeds.osm.pbf",
+  extra_tags = required_tags
+)
+
+test3 = oe_read(
+  file_path = "/home/james/Desktop/r_dl_tests/bbbike_Leeds.osm.pbf",
+  extra_tags = required_tags
+)
+
+leeds_lines_network = oe_get(
+  place = region_name,
+  layer = "lines",
+  extra_tags = required_tags,
+  boundary = leeds_buffer,
+  boundary_type = "clipsrc",
+  force_download = TRUE
+  #never_skip_vectortranslate = TRUE
+)
+# Remove NA highways (waterways, railways, aerialways, etc.)
+leeds_lines_network = leeds_lines_network %>% dplyr::filter(! is.na(highway))
+
+leeds_pois_network = osmextract::oe_get(
+  palce= region_name,
+  layer = "points",
+  extra_tags = required_tags,
+  boundary = leeds_buffer,
+  boundary_type = "clipsrc",
+  force_download = TRUE,
+  never_skip_vectortranslate = TRUE
+)
+# Data processing ---------------------------------------------------------
+
+
 
 # GeoJson of Leeds network obtained with osmextract 2/07/2022.
 a_test_network = sf::read_sf("https://github.com/udsleeds/openinfra/releases/download/v0.2/Leeds.geojson")
@@ -47,24 +107,24 @@ names(a_test_network)
 
 # Upload data -------------------------------------------------------------
 
-data_pack_basename = paste0("datapack_", region_name)
-data_pack_basename
-formats = c(".geojson", ".gpkg")
-for (f in formats) {
-  data_pack_filename = paste0(data_pack_basename, f)
-  message("Writing data for ", region_name, ": ", data_pack_filename)
-  sf::write_sf(a_test_network, data_pack_filename)
-  message("Uploading data for ", region_name, ": ", data_pack_filename)
-  piggyback::pb_upload(data_pack_filename)
-}
+#data_pack_basename = paste0("datapack_", region_name)
+#data_pack_basename
+#formats = c(".geojson", ".gpkg")
+#for (f in formats) {
+#  data_pack_filename = paste0(data_pack_basename, f)
+#  message("Writing data for ", region_name, ": ", data_pack_filename)
+#  sf::write_sf(a_test_network, data_pack_filename)
+#  message("Uploading data for ", region_name, ": ", data_pack_filename)
+#  piggyback::pb_upload(data_pack_filename)
+#}
 # create shapefile (not by default)
-data_pack_filename_shp = paste0(data_pack_basename, ".shp")
-dir.create(paste0(data_pack_basename, "_shp"))
-sf::write_sf(a_test_network, file.path(paste0(data_pack_basename, "_shp"), data_pack_filename_shp))
-waldo::compare(names(a_test_network), names(a_test_shp))
-a_test_shp = sf::read_sf("datapack_leeds_shp/datapack_leeds.shp")
-zip(zipfile = paste0(data_pack_basename, ".zip"), files = paste0(data_pack_basename, "_shp"))
-piggyback::pb_upload(paste0(data_pack_basename, ".zip"))
+#data_pack_filename_shp = paste0(data_pack_basename, ".shp")
+#dir.create(paste0(data_pack_basename, "_shp"))
+#sf::write_sf(a_test_network, file.path(paste0(data_pack_basename, "_shp"), data_pack_filename_shp))
+#waldo::compare(names(a_test_network), names(a_test_shp))
+#a_test_shp = sf::read_sf("datapack_leeds_shp/datapack_leeds.shp")
+#zip(zipfile = paste0(data_pack_basename, ".zip"), files = paste0(data_pack_basename, "_shp"))
+#piggyback::pb_upload(paste0(data_pack_basename, ".zip"))
 
 #___________MAPS_____________________________
 
