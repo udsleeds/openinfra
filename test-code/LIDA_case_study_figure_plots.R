9dc8c5979746534676ed906e9efa00cd9a1271dd
hulsiejames <hulse.jjc@sky.com>
2022-09-23 16:24:24 +0100

    included figuire for presentation to new cohort of data scientists

--- test-code/LIDA_case_study_figure_plots.R
+++ test-code/LIDA_case_study_figure_plots.R
@@ -45,3 +45,21 @@ figure_2 = tmap::tm_shape(output) +
 tmap::tmap_save(figure_2, paste0(fig_dir, "Figure 2 Openinfra recategorised OSM highway values, 2 km circular buffer around Leeds City Centre.jpg"),
                 dpi = dpi)
 
+
+# oi_cycle_separation plot ------------------------------------------------
+
+output = openinfra::oi_cycle_separation(data, remove = TRUE)
+
+presentation_figure = tmap::tm_shape(output) + 
+  tmap::tm_lines(col = "openinfra_cycle_infra") + 
+  tmap::tm_add_legend(border.col = "black",
+                      border.lwd = 1) + 
+  tmap::tm_layout(main.title = "Openinfra recategorised cycling infrastructure ",
+                  main.title.position = "centre",
+                  main.title.size = 1,
+                  legend.outside = TRUE,
+                  frame = FALSE,
+                  legend.frame = TRUE)
+
+tmap::tmap_save(presentation_figure, paste0(fig_dir, "Cycling_infra_leeds.jpg"),
+                dpi = dpi)
