565add70a54908b5f8c4d171ee88dab997d47724
hulsiejames <hulse.jjc@sky.com>
2022-10-14 14:55:03 +0100

    updated script for new data docs

--- R/data.R
+++ R/data.R
@@ -11,7 +11,7 @@
 #' OSM infrastructure data used in SOTM2022 workshop & figures for SOTM2022. 
 #' @docType data
 #' @keywords dataset
-#' @format A data frame of OSM ways.
+#' @format A data frame of OSM ways within a 2.5km radius of Leeds City Centre.
 #' @source {[generate_example_data.R](https://github.com/udsleeds/openinfra/blob/main/test-code/generate_example_data.R)}
 #' @examples
 #' library(sf)
@@ -21,9 +21,32 @@
 #' OSM points data used in the creation of transport infrastructure data packs.
 #' @docType data
 #' @keywords dataset
-#' @format A dat frame of OSM ways.
+#' @format A data frame of OSM nodes within a 2.5km radius of Leeds City Centre.
 #' @source {[generate_example_data.R](https://github.com/udsleeds/openinfra/blob/main/test-code/generate_example_data.R)}
 #' @examples 
 #' library(sf)
 #' colnames(example_data_pois)
 "example_data_pois"
+
+#' OSM infrastructure data used in SOTM2022 workshop & figures for SOTM2022. 
+#' @docType data
+#' @keywords dataset
+#' @format A data frame of OSM relations within a 2.5km radius of Leeds City
+#'   Centre.
+#' @source {[generate_example_data.R](https://github.com/udsleeds/openinfra/blob/main/test-code/generate_example_data.R)}
+#' @examples
+#' library(sf)
+#' colnames(example_data_rels)
+"example_data_rels"
+
+#' OSM infrastructure data used in SOTM2022 workshop & figures for SOTM2022. 
+#' @docType data
+#' @keywords dataset
+#' @format A data frame of OSM other relations within a 2.5km radius of Leeds 
+#'   City Centre.
+#' @source {[generate_example_data.R](https://github.com/udsleeds/openinfra/blob/main/test-code/generate_example_data.R)}
+#' @examples
+#' library(sf)
+#' colnames(example_data_other_rels)
+"example_data_other_rels"
+
